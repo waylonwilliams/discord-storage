f610fcbad17b2519b2fb64849325eca7db282a4e
Waylon Williams <crazyocean9@gmail.com>
2024-02-17 15:33:10 -0800

    accepts file upload, need to parse

--- back-end/src/App.ts
+++ back-end/src/App.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import formidable from "formidable";
+import fs from "fs";
 
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.put("/upload", (req: Request, res: Response) => {
+  const file = req.body;
+  console.log(file);
+  res.status(200).json({ message: "File uploaded successfully" });
 });
 
 app.listen(5000, () => {
