01bff797ea4b3d211374596da5b39cf20eb5651c
Waylon Williams <crazyocean9@gmail.com>
2024-02-18 13:30:49 -0800

    files uploaded to backend

--- back-end/src/App.ts
+++ back-end/src/App.ts
@@ -1,15 +1,22 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import formidable from "formidable";
-import fs from "fs";
+import fileUpload, { FileArray, UploadedFile } from "express-fileupload";
+import * as path from "path";
 
+const uploadedPath = path.join(__dirname, "../uploaded");
 const app = express();
 app.use(cors());
+app.use(fileUpload());
 
 app.put("/upload", (req: Request, res: Response) => {
-  const file = req.body;
-  console.log(file);
-  res.status(200).json({ message: "File uploaded successfully" });
+  if (req.files !== null && req.files !== undefined) {
+    const file: UploadedFile = Array.isArray(req.files.file)
+      ? req.files.file[0]
+      : req.files.file;
+    console.log(file);
+    file.mv(path.join(uploadedPath, file.name));
+    res.status(200).json({ message: "File uploaded successfully" });
+  }
 });
 
 app.listen(5000, () => {
