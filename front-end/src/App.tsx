fa10053aa08957c476e8e9e717c0a17caec1ec14
waylonwilliams <crazyocean9@gmail.com>
2024-04-04 17:21:34 -0700

    fixed init values

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,16 +4,38 @@ import LeftColumn from "./LeftColumn.tsx";
 import RightColumn from "./RightColumn.tsx";
 
 function App() {
-  let tempFilesArr = [];
-  const ls = { ...localStorage }; // { file: csv of ids }
-  for (const key in ls) {
-    tempFilesArr.push({
-      file: key,
-      ids: ls[key],
-    });
+  // FOR DEBUGGING
+  if (localStorage.getItem("Home") === null) {
+    localStorage.setItem(
+      "Home",
+      JSON.stringify([
+        { file: "Filename", ids: ["1111", "2222", "3333", "4444"] },
+        { file: "File1", ids: ["1111", "2222", "3333", "4444"] },
+        { file: "File2", ids: ["1111", "2222", "3333", "4444"] },
+      ])
+    );
   }
-  const [files, setFiles] = useState<fileArrayElement[]>(tempFilesArr); // [ {file: fileName, ids: csv of message ids} ]
-  const [folders, setFolders] = useState<string[]>(["Home", "Trash"]);
+  if (localStorage.getItem("Trash") === null) {
+    localStorage.setItem(
+      "Trash",
+      JSON.stringify([
+        { file: "Filename", ids: ["1111", "2222", "3333", "4444"] },
+        { file: "File1", ids: ["1111", "2222", "3333", "4444"] },
+        { file: "File2", ids: ["1111", "2222", "3333", "4444"] },
+      ])
+    );
+  }
+
+  let tempFoldersArr = [];
+  const ls = { ...localStorage }; // { folder: [], folder: [] }
+  for (const folder in ls) {
+    if (folder !== "Home" && folder !== "Trash") {
+      tempFoldersArr.push(folder);
+    }
+  }
+
+  const [files, setFiles] = useState<fileArrayElement[]>(ls["Home"]);
+  const [folders, setFolders] = useState<string[]>(tempFoldersArr);
   const [selectedFolder, setSelectedFolder] = useState<string>("Home");
   const [uploading, setUploading] = useState<string[]>([]);
   const [downloading, setDownloading] = useState<string[]>([]);
