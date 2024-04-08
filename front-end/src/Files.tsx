9197e0e19767b0238c5930df5b0d23b976e466f2
waylonwilliams <crazyocean9@gmail.com>
2024-04-08 00:05:05 -0700

    empty folder notice

--- front-end/src/Files.tsx
+++ front-end/src/Files.tsx
@@ -37,50 +37,57 @@ export default function Files({
 
   return (
     <>
-      <div className="grid grid-cols-6 m-2 cursor-default">
-        {Object.keys(files).map((file: string, index) => (
-          // outer div to center in column, inner div is each file box
-          <div
-            className="flex justify-center"
-            onContextMenu={(e) => {
-              e.preventDefault(); // disables default context menu
-              if (e.pageX > screen.width - 200) {
-                // makes sure the context menu is in range nicely
-                setContextMenuPoints({
-                  x: e.pageX - 175,
-                  y: e.pageY,
-                });
-              } else {
-                setContextMenuPoints({
-                  x: e.pageX,
-                  y: e.pageY,
-                });
-              }
-              setRightClickedFile(file);
-              setRightClicked(true);
-            }}
-            key={index}
-          >
-            <div className="bg-purple-1 w-44 h-44 m-4 text-center flex flex-col items-center hover:bg-purple-2 rounded-lg text-sm">
-              <div className="truncate max-w-36 pt-2">{file}</div>
-              <img className="w-24 h-24 mt-4" src="/assets/file.png" />
+      {Object.keys(files).length == 0 ? (
+        <div className="flex justify-center items-center h-screen">
+          Temporary cat for when a folder is empty
+          <img src="assets/cat.png" className=" max-w-xs max-h-xs" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-6 m-2 cursor-default">
+          {Object.keys(files).map((file: string, index) => (
+            // outer div to center in column, inner div is each file box
+            <div
+              className="flex justify-center"
+              onContextMenu={(e) => {
+                e.preventDefault(); // disables default context menu
+                if (e.pageX > screen.width - 200) {
+                  // makes sure the context menu is in range nicely
+                  setContextMenuPoints({
+                    x: e.pageX - 175,
+                    y: e.pageY,
+                  });
+                } else {
+                  setContextMenuPoints({
+                    x: e.pageX,
+                    y: e.pageY,
+                  });
+                }
+                setRightClickedFile(file);
+                setRightClicked(true);
+              }}
+              key={index}
+            >
+              <div className="bg-purple-1 w-44 h-44 m-4 text-center flex flex-col items-center hover:bg-purple-2 rounded-lg text-sm">
+                <div className="truncate max-w-36 pt-2">{file}</div>
+                <img className="w-24 h-24 mt-4" src="/assets/file.png" />
+              </div>
             </div>
-          </div>
-        ))}
-        {rightClicked && (
-          <ContextMenu
-            x={contextMenuPoints.x}
-            y={contextMenuPoints.y}
-            rightClickedFile={rightClickedFile}
-            setFiles={setFiles}
-            downloading={downloading}
-            setDownloading={setDownloading}
-            files={files}
-            selectedFolder={selectedFolder}
-            setFolders={setFolders}
-          />
-        )}
-      </div>
+          ))}
+          {rightClicked && (
+            <ContextMenu
+              x={contextMenuPoints.x}
+              y={contextMenuPoints.y}
+              rightClickedFile={rightClickedFile}
+              setFiles={setFiles}
+              downloading={downloading}
+              setDownloading={setDownloading}
+              files={files}
+              selectedFolder={selectedFolder}
+              setFolders={setFolders}
+            />
+          )}
+        </div>
+      )}
     </>
   );
 }
